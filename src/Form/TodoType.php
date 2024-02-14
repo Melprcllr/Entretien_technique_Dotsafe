62d086ddc167df0603bebae7dbf52dba2a5325ed
Melprcllr <m.parceiller@free.fr>
2024-02-14 10:52:00 +0100

    add homepage style + new todo

--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -6,6 +6,7 @@ use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TodoType extends AbstractType
 {
@@ -13,7 +14,13 @@ class TodoType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('details')
+            ->add('details', TextareaType::class, [
+                'label' => 'Détails',
+                'attr' => [
+                    'cols' => 50,
+                    'rows' => 10,
+                ]
+            ])
             ->add('completed')
         ;
     }
