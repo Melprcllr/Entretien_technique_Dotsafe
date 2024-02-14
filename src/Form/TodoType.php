7772da6e71437fb3c9e66b8f100fd0d160d84bc3
Melprcllr <m.parceiller@free.fr>
2024-02-14 11:54:02 +0100

    bootstrap new Todo, update Todo and show Todo

--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -13,15 +15,28 @@ class TodoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom*',
+                'attr' => [
+                'placeholder' => 'Indiquez votre nom...'
+                ]
+            ])
+
             ->add('details', TextareaType::class, [
-                'label' => 'Détails',
+                'label' => 'Détails*',
                 'attr' => [
                     'cols' => 50,
                     'rows' => 10,
+                    'placeholder' => 'Détaillez votre todo...',
                 ]
             ])
-            ->add('completed')
+            ->add('completed', ChoiceType::class, [
+                'label' => 'La Todo est-elle complète ?*',
+                'choices' => [
+                    'Oui' => 'Option 1',
+                    'Non' => 'Option 2',
+                ],
+        ])
         ;
     }
 
